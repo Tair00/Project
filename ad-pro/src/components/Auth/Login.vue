0bf486b5ba82db71fd81a499dc7b206920ff0bb5
Tair00 <suleimanov.t.i.20@gmail.com>
2021-11-27 22:07:51 +0300

    br_3_4

--- ad-pro/src/components/Auth/Login.vue
+++ ad-pro/src/components/Auth/Login.vue
@@ -3,34 +3,39 @@
 		<v-layout align-center justify-center> 
 			<v-flex xs12 sm8 md8>
 			<v-card class="elevetion-12">
-				<v-toolbar dark color="primary">
-                    <v-toolbar-title>Login
-                    </v-toolbar-title>
-                    </v-toolbar>
+			<v-toolbar dark color="primary">
+				<v-toolbar-title>Login</v-toolbar-title>
+			</v-toolbar>
 				<v-card-text>
-                    <v-form v-model="valid" ref="form" 
-                    validation>
+					<v-form v-model="valid" ref="form" validation>
 					<v-text-field
-						prepend-icon="mdi-account" 							
-						name="email" 
-						label="Email" 
-						type="email"
-                        v-model="email" 
-                        :rules="emailRules" >
+					prepend-icon="mdi-account" 
+					name="email" 
+					label="Email" 
+					type="email" 
+					v-model="email"
+					:rules="emailRules" 
+					>
 					</v-text-field>
-					<v-text-field
-						prepend-icon="mdi-lock" 
-						name="password" 
-						label="Password" 
-						type="password"
-                        v-model="password" 
-                        :rules="passwordRules">
-					</v-text-field>
+					<v-text-field  
+					prepend-icon="mdi-lock" 
+					name="password" 
+					label="Password" 
+					type="password" 
+					v-model="password"
+					:rules="passwordRules" 
+					></v-text-field>
 					</v-form> 
 				</v-card-text>
 				<v-card-actions>
 					<v-spacer></v-spacer>
-					<v-btn color="primary">Login</v-btn>
+					<v-btn 
+					color="primary"
+					@click="onSubmit"
+					:disabled="!valid"
+					>
+						Login
+					</v-btn>
 				</v-card-actions>	
 			</v-card>
 			</v-flex>
@@ -40,20 +45,29 @@
 
 <script>
 export default {
- data () { 
+	data () { 
 		return {
 			email: "",
 			password: "",
 			valid: false,
 			emailRules: [
                 v => !!v || 'E-mail is required',
-                v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
-			],
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid'],
 			passwordRules: [
                 v => !!v || 'Password is required',
                 v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters']
 		} 	
+	},
+	methods: {
+		onSubmit(){
+			if (this.$refs.form.validate()){
+				const user = {
+					email: this.email,
+					password: this.password
+				}
+				console.log(user)
+			}
+		}
 	}
-
 } 
 </script>
