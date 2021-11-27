52faa061394c7df4a090ce767114626e32ca17b4
Tair00 <suleimanov.t.i.20@gmail.com>
2021-11-27 23:04:58 +0300

    br_4_2

--- ad-pro/src/components/Auth/Registration.vue
+++ ad-pro/src/components/Auth/Registration.vue
@@ -25,6 +25,15 @@
 					v-model="password"
 					:rules="passwordRules" 
 					></v-text-field>
+
+					<v-text-field  
+					prepend-icon="mdi-lock" 
+					name="confirm-password" 
+					label="Confirm Password" 
+					type="password" 
+					v-model="confirmPassword"
+					:rules="confirmPasswordRules" 
+					></v-text-field>
 					</v-form> 
 				</v-card-text>
 				<v-card-actions>
@@ -49,13 +58,19 @@ export default {
 		return {
 			email: "",
 			password: "",
+			confirmPassword: "",
 			valid: false,
 			emailRules: [
                 v => !!v || 'E-mail is required',
                 v => /.+@.+\..+/.test(v) || 'E-mail must be valid'],
 			passwordRules: [
                 v => !!v || 'Password is required',
-                v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters']
+                v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'],
+			confirmPasswordRules: [
+				v => !!v || 'Password is required',
+				v => v === this.password || 'Password should match'
+]
+
 		} 	
 	},
 	methods: {
