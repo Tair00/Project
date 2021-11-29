bc34cf36f4de6c8e1f21410e4597e699b2720ad5
Tair00 <suleimanov.t.i.20@gmail.com>
2021-11-29 22:10:03 +0300

    br_5_4

--- ad-pro/src/App.vue
+++ ad-pro/src/App.vue
@@ -1,127 +1,57 @@
-<template>
-
-<v-app>
-  <!-- Layout menu in drawer -->
-  <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            КИПУ
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Учебный проект
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-
-
-
-
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item-group color="primary">
-          <v-list-item
- v-for="link in links"
- :key="link.title"
- :to="link.url"
->
-
-            <v-list-item-icon>
-              <v-icon>{{ link.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ link.title }}</v-list-item-title>
-            </v-list-item-content>
-      </v-list-item>
-      
-</v-list-item-group>
-
-      </v-list>
-  </v-navigation-drawer> 
-
-<div>
+<template> 
+	<div>
 	<v-container fluid>
 		<v-layout row> 
 			<v-flex xs12>
 				<v-carousel>
-					<v-carousel-item></v-carousel-item>
+					<v-carousel-item
+					v-for="ad in ads"
+					:key="ad.id"
+					:src="ad.src"
+					>
+						<div class="ad-link">
+							<v-btn class="error" :to="'/ad/' + ad.id">
+								{{ ad.title }}
+							</v-btn>
+						</div>
+					</v-carousel-item>
 				</v-carousel>
 			</v-flex>
 		</v-layout> 
 	</v-container>
-	<v-container>
-		<v-layout row> 
-			<v-flex xs12>
+	<v-container grid-list-lg>
+		<v-layout row wrap> 
+			<v-flex xs12 sm6 md4
+			v-for="ad in ads"
+			:key="ad.id"
+			>
 				<v-card>
 					<v-img
+					:src="ad.src"
 					height="200px"
 					></v-img>
 					<v-card-title primary-title>
-					<div>
-						<h3 
-class="headline mb0"
->Здесь будет заголовок</h3>
-						<div>Здесь будет описание</div>
-					</div>
+						<div>
+						<h3 class="headline mb-0">{{ad.title}}</h3>
+						<div>{{ad.desc}}</div>
+						</div>
 					</v-card-title>
 					<v-card-actions>
 					<v-spacer></v-spacer>
-					<v-btn text>Open</v-btn>
+					<v-btn text :to="'/ad/' + ad.id">
+						Open
+					</v-btn>
 					<v-btn raised color="primary">
 						Buy
 					</v-btn>
-					</v-card-actions>				
+				</v-card-actions>	
+					
 				</v-card>
 			</v-flex>
 		</v-layout> 
 	</v-container>
 	</div>
-
-
-
-
-    <v-app-bar-nav-icon @click = 'drawer = !drawer'></v-app-bar-nav-icon>
-    
-    <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-toolbar-title>
-      <router-link to="/" tag="span" class="pointer">Home</router-link>
-    </v-toolbar-title>
-
-    <v-spacer></v-spacer>
-
-    <v-toolbar-items class="hidden-sm-and-down">
-       <!-- <v-btn 
-       v-for="link in links" 
-       :key="link.title" 
-       text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>  -->
-       <v-btn 
- v-for="link in links"
- :key="link.title"
- :to="link.url" 
- text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
-
-    </v-toolbar-items>
-
-        </v-app-bar>
-       
-  <v-content>
-    <router-view></router-view> 
-  </v-content>
-  </v-app>
-
 </template>
-
-
-<style scoped>
-  .pointer {
-    cursor: pointer;
-  }
-</style>
-
 <script>
 export default {
 	data () { 
@@ -161,5 +91,17 @@ export default {
 	}
 } 
 </script>
+<style scoped>
+	.ad-link {
+		position:absolute;
+		bottom: 50px;
+		left:50%;
+		background: rgb(0,0,0,0.5);
+		transform: translate(-50%,0);
+		padding: 5px 15px;
+		border-top-right-radius: 5px;
+		border-top-left-radius: 5px;
+	}
+</style>
 
-	
+
