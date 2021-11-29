db72753f528226b8f9b9723cae5c68b5c1c4d435
Tair00 <suleimanov.t.i.20@gmail.com>
2021-11-29 21:51:14 +0300

    br_5_2

--- ad-pro/src/App.vue
+++ ad-pro/src/App.vue
@@ -37,6 +37,47 @@
       </v-list>
   </v-navigation-drawer> 
 
+<div>
+	<v-container fluid>
+		<v-layout row> 
+			<v-flex xs12>
+				<v-carousel>
+					<v-carousel-item></v-carousel-item>
+				</v-carousel>
+			</v-flex>
+		</v-layout> 
+	</v-container>
+	<v-container>
+		<v-layout row> 
+			<v-flex xs12>
+				<v-card>
+					<v-img
+					height="200px"
+					></v-img>
+					<v-card-title primary-title>
+					<div>
+						<h3 
+class="headline mb0"
+>Здесь будет заголовок</h3>
+						<div>Здесь будет описание</div>
+					</div>
+					</v-card-title>
+					<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn text>Open</v-btn>
+					<v-btn raised color="primary">
+						Buy
+					</v-btn>
+					</v-card-actions>				
+				</v-card>
+			</v-flex>
+		</v-layout> 
+	</v-container>
+	</div>
+
+
+
+
     <v-app-bar-nav-icon @click = 'drawer = !drawer'></v-app-bar-nav-icon>
     
     <v-app-bar app dark color="primary">
@@ -68,6 +109,8 @@
   </v-app>
 
 </template>
+
+
 <style scoped>
   .pointer {
     cursor: pointer;
@@ -91,3 +134,4 @@ export default {
     }
 }
 </script>
+	
