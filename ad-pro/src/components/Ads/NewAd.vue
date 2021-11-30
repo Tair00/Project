ae251c3c58ce33eff5aa02355a135e9618fab964
Tair00 <suleimanov.t.i.20@gmail.com>
2021-11-30 19:43:05 +0300

    br_7_1

--- ad-pro/src/components/Ads/NewAd.vue
+++ ad-pro/src/components/Ads/NewAd.vue
@@ -2,10 +2,30 @@
     <v-container>
         <v-layout>
             <v-flex xs12>
-                <h1>NewAd</h1>
+                <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
+                <v-form v-model="valid" ref="form" validation>
+	<v-text-field
+	name="title" 
+	label="Ad Title" 
+	type="text" 
+	v-model="title"
+	:rules="[ v => !!v || 'Title is required']" 
+	>
+	</v-text-field>
+	<v-textarea  
+	name="description" 
+	label="Ad Description" 
+	type="text" 
+	v-model="description"
+	:rules="[ v => !!v || 'Description is required']"
+	class="mb-3" 
+	></v-textarea>
+</v-form>
+
             </v-flex>
         </v-layout>
     </v-container>
+    
 </template>
 
 <script>
