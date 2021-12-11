8cf2b93169bc8b6a25efb8be6007c53556d1a9df
Tair00 <suleimanov.t.i.20@gmail.com>
2021-12-11 19:09:07 +0300

    br_11_3

--- ad-pro/src/components/Ads/NewAd.vue
+++ ad-pro/src/components/Ads/NewAd.vue
@@ -3,13 +3,14 @@
 		<v-layout row> 
 			<v-flex xs12 sm6 offset-sm3>
 				<h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
-				<v-form v-model="valid" ref="form" validation> 
+				<v-form v-model="valid" ref="form" validation>
 					<v-text-field
 					name="title" 
 					label="Ad Title" 
 					type="text" 
 					v-model="title"
-					:rules="[ v => !!v || 'Title is required']">
+					:rules="[ v => !!v || 'Title is required']" 
+					>
 					</v-text-field>
 					<v-textarea  
 					name="description" 
@@ -21,43 +22,41 @@
 					></v-textarea>
 					</v-form> 
 					<v-layout  row> 
-						<v-flex xs12>
-						<v-btn 
-						class="mt-3"
-						color="warning">
-						Upload
-                            <v-icon right dark>mdi-cloud-
-                            upload</v-icon>
-						</v-btn>
-						</v-flex>
+							<v-flex xs12>
+							<v-btn 
+							class="mt-3"
+							color="warning">
+								Upload
+								<v-icon right dark>mdi-cloud-upload</v-icon>
+							</v-btn>
+							</v-flex>
 					</v-layout> 
-					<v-layout row>  					
-						<v-flex xs12>
-						<img src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" height="150" class="mt-3">
-						</v-flex>
+					<v-layout row> 
+							<v-flex xs12>
+								<img src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" height="150" class="mt-3">
+							</v-flex>
+					</v-layout>
+					<v-layout row> 
+							<v-flex xs12>
+								<v-switch
+										v-model="promo"
+										label="Ad to Promo?"
+									></v-switch>
+							</v-flex>
 					</v-layout> 
-					<v-layout row> 					
-						<v-flex xs12>
-						<v-switch
-                            v-model="promo"
-                            label="Ad to Promo?"
-                                ></v-switch>
-						</v-flex>
-					</v-layout> 
-					<v-layout row>  
-						<v-flex xs12>
-						<v-spacer></v-spacer>
-						<v-btn 
-						color="success"
-						@click="createAd" 
-						>Create Ad</v-btn>
-						</v-flex>
-					</v-layout>  
+					<v-layout row> 
+							<v-flex xs12>
+								<v-spacer></v-spacer>
+								<v-btn 
+								color="success"
+								@click="createAd" 
+								>Create Ad</v-btn>
+							</v-flex>
+					</v-layout>   
 			</v-flex>
 		</v-layout>
 	</v-container>
 </template>
-
 <script>
 export default {
 	data () { 
@@ -72,15 +71,14 @@ export default {
 		createAd(){
 			if (this.$refs.form.validate()){
 			const ad = {
-			title: this.title,
-			desc: this.description,
-			promo: this.promo,
-			src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
+				title: this.title,
+				desc: this.description,
+				promo: this.promo,
+				src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
 			}
-			console.log(ad)
-	}
-}
-
+			this.$store.dispatch("createAd", ad)
+			}
+		}
 	}
 } 
 </script>
