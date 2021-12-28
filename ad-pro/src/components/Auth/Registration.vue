e0fdccbc961e1cbe5b19c4d57b46572a24286558
Tair00 <suleimanov.t.i.20@gmail.com>
2021-12-28 23:43:05 +0300

    br_14_4

--- ad-pro/src/components/Auth/Registration.vue
+++ ad-pro/src/components/Auth/Registration.vue
@@ -75,14 +75,23 @@ export default {
 	},
 	methods: {
 		onSubmit(){
-			if (this.$refs.form.validate()){
-				const user = {
-					email: this.email,
-					password: this.password
-				}
-				console.log(user)
+	if (this.$refs.form.validate()){
+	const user = {
+		email: this.email,
+		password: this.password
+	}
+	this.$store.dispatch('registerUser', user)
+	.then(() => {
+		this.$router.push("/")
+	})
+	.catch((err) => {
+		console.log(err.message)
+	})
+}
+
+
 			}
 		}
 	}
-} 
+
 </script>
