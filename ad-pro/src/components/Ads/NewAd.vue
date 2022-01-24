f90d1cc9a0159f6d8f0bfafa70b6d87c0032e709
Tair00 <suleimanov.t.i.20@gmail.com>
2022-01-24 17:37:21 +0300

    br_17_3

--- ad-pro/src/components/Ads/NewAd.vue
+++ ad-pro/src/components/Ads/NewAd.vue
@@ -3,7 +3,6 @@
 		<v-layout row> 
 			<v-flex xs12 sm6 offset-sm3>
 				<h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
-
 				<v-form v-model="valid" ref="form" validation>
 					<v-text-field
 					name="title" 
@@ -13,7 +12,6 @@
 					:rules="[ v => !!v || 'Title is required']" 
 					>
 					</v-text-field>
-
 					<v-textarea  
 					name="description" 
 					label="Ad Description" 
@@ -22,48 +20,45 @@
 					:rules="[ v => !!v || 'Description is required']"
 					class="mb-3" 
 					></v-textarea>
-
 					</v-form>
-
-                        <v-layout  row>
-                            <v-flex xs12>
-                            <v-btn 
-                            class="mt-3"
-                            color="warning">
-                            Upload<v-icon right dark>mdi-cloud-upload</v-icon>
-                            </v-btn>
-                            </v-flex>
-                        </v-layout>
-
-                        <v-layout row>
-                            <v-flex xs12>
-                            <img 	src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" height="150" class="mt-3">
-                            </v-flex>
-                        </v-layout>
-
-                        <v-layout row>
-                            <v-flex xs12>
-                            <v-switch
-                            v-model="promo"
-                            label="Ad to Promo?"
-                                ></v-switch>
-                            </v-flex>
-                        </v-layout>
-
-                        <v-layout row>
-                            <v-flex xs12>
-                            <v-spacer></v-spacer>
-                            <v-btn 
-                            color="success"
-                            @click="createAd" 
-                            >Create Ad</v-btn>
-                            </v-flex>
-                        </v-layout>
+					<v-layout  row>
+						<v-flex xs12>
+						<v-btn 
+						class="mt-3"
+						color="warning">
+						Upload
+						<v-icon right dark>mdi-cloud-upload</v-icon>
+						</v-btn>
+						</v-flex>
+					</v-layout>
+					<v-layout row>
+						<v-flex xs12>
+						<img 	src="https://images.greelane.com/proxy?url=https%3A%2F%2Fwww.thoughtco.com%2Fthmb%2FbGQ3QmmPJ_mEWqdObcA_iHCByrw%3D%2F1500x1000%2Ffilters%3Afill%28auto%2C1%29%2FGettyImages-612789276-5c76bafc46e0fb00019b8cf9.jpg&width=750" height="150" class="mt-3">
+						</v-flex>
+					</v-layout>
+					<v-layout row>
+						<v-flex xs12>
+						<v-switch
+						v-model="promo"
+						label="Ad to Promo?"
+						></v-switch>
+						</v-flex>
+					</v-layout>
+					<v-layout row>
+						<v-flex xs12>
+						<v-spacer></v-spacer>
+						<v-btn 
+						color="success"
+						@click="createAd"
+						:loading="loading"
+						:disabled:="!valid || loading" 
+						>Create Ad</v-btn>
+						</v-flex>
+					</v-layout>
 			</v-flex>
 		</v-layout>
 	</v-container>
 </template>
-
 <script>
 export default {
 	data () { 
@@ -76,17 +71,30 @@ export default {
 	},
 	methods: {
 		createAd(){
-			if (this.$refs.form.validate())
-			{
-				const ad = {
-					title: this.title,
-					desc: this.description,
-					promo: this.promo,
-					src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
-				}
-			this.$store.dispatch("createAd", ad)
+			if (this.$refs.form.validate()){
+			const ad = {
+				title: this.title,
+				desc: this.description,
+				promo: this.promo,
+				src: "https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/rtx_3090_21_gbps_memory_micron.png"
 			}
-		}
+			this.$store.dispatch("createAd", ad)
+			.then(() => {
+			this.$router.push("/list")
+			})
+			.catch(() => {})
+
+
+			}
+		},
+		computed: {
+			loading() {
+			return this.$store.getters.loading
 	}
+},
+
+	}
+
+
 } 
 </script>
